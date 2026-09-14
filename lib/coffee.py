b7efdfa38d21ad6b7d3dba5cb86ae814272fa9d5
sienna75 <sienna.ngangastudent.moringaschool.com>
2026-09-14 15:45:32 -0700

    Pass all coffee tests

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,5 +1,26 @@
 class Coffee:
-    VALID_SIZES = ["small", "medium", "large"]
+    VALID_SIZES = ["small", "medium", "large", "Small", "Medium", "Large"]
+
+    def __init__(self, size="small", price=0.0):
+        self.size = size
+        self.price = float(price)
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, str) and value.lower() in ["small", "medium", "large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+            if not hasattr(self, "_size"):
+                self._size = "small"
+
+    def tip(self, amount=1):
+        self.price += float(amount)
+        print("This coffee is delicious, here’s a tip!")    VALID_SIZES = ["small", "medium", "large"]
 
     def __init__(self, size="small", price=0.0):
         self.size = size
