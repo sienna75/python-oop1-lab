a313482d69ca00ba4e8da3c639cbe17cb945da33
sienna75 <sienna.ngangastudent.moringaschool.com>
2026-09-14 15:47:40 -0700

    Fix syntax error and finish coffee class

--- lib/coffee.py
+++ lib/coffee.py
@@ -20,24 +20,4 @@ class Coffee:
 
     def tip(self, amount=1):
         self.price += float(amount)
-        print("This coffee is delicious, here’s a tip!")    VALID_SIZES = ["small", "medium", "large"]
-
-    def __init__(self, size="small", price=0.0):
-        self.size = size
-        self.price = float(price)
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, value):
-        if isinstance(value, str) and value.lower() in self.VALID_SIZES:
-            self._size = value.lower()
-        else:
-            print("size must be small, medium, or large")
-            if not hasattr(self, "_size"):
-                self._size = "small"
-
-    def tip(self, amount=1):
-        self.price += float(amount)
+        print("This coffee is delicious, here’s a tip!")
